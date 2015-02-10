8f4a08e4a1b212edf629e213153fc5004a83d140
Ahmad Draidi <ar2000jp@gmail.com>
2015-02-10 23:12:02 +0200

    Add full playbook generator script Added a script that generates a playbook that has all roles, and     all default variables.

---
+++ scripts/genFullPlaybook.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+cat > fullPlaybook.yml <<EOF
+---
+### AUTO-GENERATED ###
+- hosts: all
+  pre_tasks:
+    - shell: echo "Last provisioning started at \$(date)" > {{ provision__base_dir }}/provisioning.txt
+  post_tasks:
+    - shell: echo "Last provisioning completed at \$(date)" >> {{ provision__base_dir }}/provisioning.txt
+
+  roles:
+EOF
+
+for r in $(find roles/ -maxdepth 1 -mindepth 1 -type d)
+  do
+  f=$(basename $r);
+  echo "    - role: $f" >> fullPlaybook.yml
+done
+
+echo "" >> fullPlaybook.yml
+echo "  vars:" >> fullPlaybook.yml
+
+(for r in $(ls roles/) ;
+    do 
+    if [ -f roles/$r/defaults/main.yml ];
+        then echo "" ;
+	echo "    ## $r role" ;
+	cat roles/$r/defaults/main.yml | grep -v '^---' | grep -v '^[[:space:]]*$' | sed 's,^[[:space:]]*,    ,';
+    fi
+ done) >> fullPlaybook.yml
